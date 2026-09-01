26b2465f0d896bd69f4c61fa39e107c818e049a5
Rohit Dhaka <rohitdhaka2110@gmail.com>
2026-09-01 12:40:51 +0530

    add otp model

---
+++ src/models/Otp.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+
+const otpSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users',
+        required:true
+    },
+    otpHash:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    type:{
+        type:String,
+        enum:['verify-email','forget-password'],
+        default:'verify-email'
+    },
+    expiredAt:{
+        type:Date,
+        default: ()=> new Date(Date.now() + 10 * 60 * 1000)
+    }
+},{timestamps:true})
+
+const otpModel = mongoose.model('otps' , otpSchema)
+export default otpModel;
