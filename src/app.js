da0434726e6dd723cbd88456353237e2430f8fb0
Rohit Dhaka <rohitdhaka2110@gmail.com>
2026-09-04 00:00:29 +0530

    change origin url

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 
 app.use(express.json())
-app.use(cors({origin:"http://localhost:5173" , credentials:true}))
+app.use(cors({origin:"https://vocabfy-beta.vercel.app/" , credentials:true}))
 app.use(cookieParser())
 
 app.get("/health", (req, res) => {
