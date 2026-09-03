3291903213791b59335743116040e82f60a455a6
Rohit Dhaka <rohitdhaka2110@gmail.com>
2026-09-04 00:30:30 +0530

    correct origin url

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 
 app.use(express.json())
-app.use(cors({origin:"https://vocabfy-beta.vercel.app/" , credentials:true}))
+app.use(cors({origin:"https://vocabfy-beta.vercel.app" , credentials:true}))
 app.use(cookieParser())
 
 app.get("/health", (req, res) => {
