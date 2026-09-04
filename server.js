78a94c9cf72c8fa9f9eec36c52e45b0233824e71
Rohit Dhaka <rohitdhaka2110@gmail.com>
2026-09-04 11:29:44 +0530

    add two origin

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ import app from './src/app.js'
 import connectDB from './src/config/db.js'
 import env from './src/config/env.js'
 
-
-
-
+const PORT = env.PORT || 3000
 
 connectDB();
-app.listen(env.PORT , ()=>{
+app.listen(PORT , ()=>{
     console.log(`app listen on port ${env.PORT}`)
 })
 
--- src/app.js
+++ src/app.js
@@ -8,7 +8,22 @@ const app = express()
 
 
 app.use(express.json())
-app.use(cors({origin:"https://vocabfy-beta.vercel.app" , credentials:true}))
+// app.use(cors({origin:"https://vocabfy-beta.vercel.app" , credentials:true}))
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://vocabfy-beta.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(cookieParser())
 
 app.get("/health", (req, res) => {
